f4a8a9894cea4f847f35688be02764f7067cbd7e
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-11-07 07:59:37 -0300

    feat: estudando statefull e stateless

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(AppWidget());
+  runApp(AppWidget(title: 'Flutterando 3',));
 }
 
 class AppWidget extends StatelessWidget{
+
+final String title;
+
+ const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+        home: HomePage()
+     );
+  } 
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+     return HomePageState();
+  }
+
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
      return Container(
-     child: Center(
-     child: Text(
-        'Hello World', 
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
-      ),
-     ),
-    );
-  } 
+          child: Center(child: GestureDetector(child: Text('Contador: $counter'), 
+            onTap:(){
+               setState(() {
+                 counter++;
+                 
+               });
+
+               print(counter);
+          }
+        )),
+      );
+  }
 }
