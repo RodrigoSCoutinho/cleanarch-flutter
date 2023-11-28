d5ccd84df6772e16f8e5743e6be02487e63c4749
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-11-28 08:38:36 -0300

    feat: refactory code and create clean arch folders

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/app_widget.dart';
+import 'package:my_app/presentation/views/home/home_page.dart';
 
-main(){
-  runApp(AppWidget());
+void main() {
+  runApp(const MyApp());
 }
 
+class Page {
+
+  builder() {
+
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Hello World!'),
+    );
+  }
+}
